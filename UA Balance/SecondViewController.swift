286bbdd4c7958ef7018971264e0d574bdb8db2f0
ganoncann0n <ganoncannn@gmail.com>
2018-04-09 15:08:07 -0500

    Initial Commit

---
+++ UA Balance/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UA Balance
+//
+//  Created by Sheldon Ruiz on 4/9/18.
+//  Copyright © 2018 Sheldon Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
